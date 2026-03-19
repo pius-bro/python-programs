a2dc54c1a7cc82963a2fcb641183b99c1b1d49b6
PIUS Programmer <osiemoconcepta@gmail.com>
2026-03-19 01:11:35 -0700

    Implement TensorFlow model for predictions
    Added a TensorFlow model to predict values based on input data.

--- machine.py
+++ machine.py
@@ -1,2 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import numpy as np
-print(np.__dict__)
+
+# Input (X) and Output (y)
+X = np.array([1, 2, 3, 4, 5], dtype=float)
+y = np.array([3, 5, 7, 9, 11], dtype=float)
+
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=[1])
+])
+model.compile(
+    optimizer='sgd',       # Stochastic Gradient Descent
+    loss='mean_squared_error'
+)
+model.fit(X, y, epochs=500)
+print(model.predict([10.0]))
